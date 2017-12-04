07aea61d5765825c687a9534431a4b4eec1d0682
“Espen <admin@altonline.no>
2017-12-04 21:01:42 +0100

    Sort high/low

--- resources/assets/js/components/Power.vue
+++ resources/assets/js/components/Power.vue
@@ -49,10 +49,11 @@
                 <p class="text-xs mt-1">Spotsprisen oppgitt er basert gjennomsnittsprisen i Norge. Oppdateres daglig. </p>
             </div>
             <p class="mt-4">
-            <button class="text-blue font-bold hover:text-blue-darker">Høyeste pris</button> |
-            <button class="text-blue font-bold hover:text-blue-darker">Laveste pris</button></p>
+            <button class="text-blue font-bold hover:text-blue-darker" @click="sortKey = 'asc'">Laveste pris</button> |
+            <button class="text-blue font-bold hover:text-blue-darker" @click="sortKey = 'desc'">Høyeste pris</button></p>
             <div class="flex flex-wrap">
-                <div class="mt-4 mb-4 mr-3 border-2 border-grey p-4 bg-grey-lightest sm:w-full md:w-2/5" v-for="vendor in vendors" :key="vendor.id">
+                <div class="mt-4 mb-4 mr-3 border-2 border-grey p-4 bg-grey-lightest sm:w-full md:w-2/5" v-for="(vendor, index) in sortedVendors" :key="vendor.id">
+                    <p class="text-sm text-green-dark" v-if="index === 0 && sortKey === 'asc'">Strømvinner</p>
                     <h3 class="text-lg font-bold text-darkest mb-3">{{ vendor.company }}</h3>
                     <p class="text-darker">Avtalenavn: {{ vendor.agreement_type }}</p>
                     <p class="text-darker">Påslag spotpris: {{ vendor.price }} kr</p>
@@ -60,7 +61,7 @@
                     <powercontact :vendordescription="vendor.description"></powercontact>
                     <div class="flex items-center bg-orange text-white text-sm font-bold px-4 py-3 mt-6" role="alert">
                         <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" fill="#FFF" /></svg>
-                        <p> Estimert pris per mnd.: {{ calculatePrice(vendor.fee, vendor.price, vendor.invoice) | roundCalculatedPrice }},-</p>
+                        <p> Estimert pris per mnd.: {{ vendor.consumer_price | roundCalculatedPrice }},-</p>
                     </div>
                     <p class="text-xs mt-1 text-darker">Vær obs på at enkelte områder i Nord-Norge er fritatt moms og andre avgifter slik
                     at den estimerte prisen kan være lavere i disse områdene</p>
@@ -87,7 +88,7 @@
             <div class="mt-4">
                 <div class="mt-4 mb-4 border-2 border-grey p-4" v-for="price in prices">
                     <h3 class="text-lg font-bold text-grey-darker mb-3">{{ price.area }}</h3>
-                    <p>Pris (kWh): {{ (price.price / 1000).toFixed(4) }}</p>
+                    <p>Pris (kWh): {{ (price.price / 1000).toFixed(4) }} eksl.mva</p>
                     <p>Sist oppdatert: {{ price.last_updated | formatDate }}</p>
                     <p>Beskrivelse: {{ price.description }}</p>
 
@@ -143,16 +144,19 @@
             }
         },
         computed: {
-
+            sortedVendors() {
+                for (let i = 0; i < this.vendors.length; i++) {
+                    this.$set(this.vendors[i], 'consumer_price', parseFloat(this.vendors[i].fee) + ((this.kwh / 12) * (parseFloat(this.spot) + (this.vendors[i].price / 100))))
+                }
+                return _.orderBy(this.vendors, 'consumer_price', this.sortKey);
+            }
         },
         mounted() {
           this.spot = this.elspotaverage;
           this.vendors = this.initialVendors;
         },
         methods: {
-            calculatePrice: function (fee, price, invoice) {
-                return parseFloat(fee) + ((this.kwh / 12) * (parseFloat(this.spot) + (price / 100)));
-            }
+           
         },
 
     }
